0f8b4dd1b6a737c91cbd53f17d319fb712253252
Booba <telegram@amazing_booba>
2021-09-16 01:31:00 +0300

    work formed and hash actually works

--- src/main.rs
+++ src/main.rs
@@ -1,23 +1,42 @@
-extern crate tiny_keccak;
-extern crate rustc_hex;
-extern crate web3;
-
-use log::debug;
+use log::{debug, info};
 
 use std::fs::File;
 use std::io::Read;
 use std::str::FromStr;
+use pretty_hex::*;
 
 use rustc_hex::{FromHex, ToHex};
 use tiny_keccak::{Hasher, Keccak};
 
-use web3::contract::{Contract, Options};
-use web3::types::{Address, Bytes, H256, U256};
+use web3::contract::Contract;
+use web3::types::{Address, Bytes, H160, H256, U256};
 
 use secp256k1::SecretKey;
 use web3::ethabi::{encode, token::Token, FixedBytes};
 
-use serde::Deserialize;
+use serde::{Deserialize, Serialize};
+
+use bincode::Options;
+use std::convert::TryInto;
+
+fn vtoa<T, const N: usize>(v: Vec<T>) -> [T; N] {
+    v.try_into()
+        .unwrap_or_else(|v: Vec<T>| panic!("Expected a Vec of length {} but it was {}", N, v.len()))
+}
+
+#[derive(Debug, Serialize, Deserialize)]
+struct Work {
+    _pad1: [u32; 7],
+    chain_id: u32,
+    entropy: [u8; 32],
+    gem_address: [u8; 20],
+    sender_address: [u8; 20],
+    _pad2: [u32; 7],
+    gem_id: u32,
+    _pad3: [u32; 7],
+    eth_nonce: u32,
+    salt: [u32; 8],
+}
 
 #[derive(Debug, Deserialize)]
 struct Network {
@@ -79,8 +98,10 @@ async fn main() -> web3::Result<()> {
     )
     .unwrap();
 
-    let tx = contract.query("gems", (config.gem_type,), config.address, Options::default(), None);
+    let tx = contract.query("gems", (config.gem_type,), config.address, web3::contract::Options::default(), None);
     /*
+    // implement From
+    // (https://stackoverflow.com/questions/53194323/is-there-any-way-of-converting-a-struct-to-a-tuple)
     #[derive(Debug)]
     struct GemInfo {
         name: String,
@@ -95,20 +116,31 @@ async fn main() -> web3::Result<()> {
     }
     */
     let gem_info: (String, String, FixedBytes, U256, U256, U256, Address, Address, Address) = tx.await.unwrap();
-    // implement From
-    // (https://stackoverflow.com/questions/53194323/is-there-any-way-of-converting-a-struct-to-a-tuple)
-    debug!("{:?}", gem_info);
-    let entropy = Token::Bytes(gem_info.2);
-    for i in 0..i64::MAX { 
-        //(chain_id, entropy, gemAddr, senderAddr, kind, nonce, salt)
-        let work = encode(&[
-            Token::Uint(chain_id),
-            entropy,
-            Token::Address(config.network.gem_address),
-            Token::Address(config.address)
-        ]); 
-        debug!("{:?}", work);
-    }
+    debug!("Got gem_info {:?}", gem_info);
+
+    let eth_nonce = web3.eth().transaction_count(config.address, None).await.unwrap();
+    //wow ethabi sucks. just spent 5+hours on figuring this stuff out
+    let work = Work {
+        _pad1: [0u32; 7],
+        chain_id: chain_id.as_u32(),
+        entropy: vtoa(gem_info.2),
+        _pad2: [0u32; 7],
+        gem_id: config.gem_type,
+        gem_address: config.network.gem_address.to_fixed_bytes(),
+        sender_address: config.address.to_fixed_bytes(),
+        _pad3: [0u32; 7],
+        eth_nonce: eth_nonce.as_u32(),
+        salt: [0u32; 8],
+    };
+    let bytes = bincode::options()
+        .with_fixint_encoding()
+        .allow_trailing_bytes()
+        .with_big_endian()
+        .serialize(&work).unwrap();
+    info!("Here is some work for you: {:02X?}", bytes.hex_dump());
+
+
+    //for i in 0..i64::MAX { }
 
     Ok(())
 }
