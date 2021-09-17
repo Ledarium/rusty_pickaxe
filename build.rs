8ac114c75811959f43c22eb1a90c1957b2401900
Booba <telegram@amazing_booba>
2021-09-17 20:15:25 +0300

    wip

--- build.rs
+++ build.rs
@@ -5,10 +5,12 @@ static CUDA_PATH: &str = "/opt/cuda/include";
 fn main() {
   cc::Build::new()
     .cuda(true)
-    .cpp(true)
     .flag("-cudart=shared")
-    .files(&["./src/cuda_keccak256.cu", "./src/keccak256.c"])
-    .compile("keccak256.a");
+    .flag("-gencode")
+    .flag("arch=compute_86,code=sm_86") // RTX 30**
+    .include("./src")
+    .files(&["./src/cuda_keccak256.cu"])
+    .compile("libcuda_keccak256.a");
   println!("cargo:rustc-link-search=native={}", CUDA_PATH);
   println!("cargo:rustc-link-search={}", CUDA_PATH);
   println!("cargo:rustc-env=LD_LIBRARY_PATH={}", CUDA_PATH);
--- src/cuda_keccak256.cu
+++ src/cuda_keccak256.cu
@@ -17,6 +17,8 @@ extern "C" {
 #define NPT 2
 #define NBN 2
 
+static bool init[MAX_GPUS] = { 0 };
+
 static uint32_t *d_nonces[MAX_GPUS];
 static uint32_t *h_nonces[MAX_GPUS];
 
@@ -193,9 +195,8 @@ void keccak256_gpu_hash_80(uint32_t threads, uint32_t startNonce, uint32_t *resN
 }
 
 __host__
-void keccak256_cpu_hash_80(int thr_id, uint32_t threads, uint32_t startNonce, uint32_t* resNonces, uint32_t target_high, uint32_t target_low)
+void keccak256_cpu_hash_80(int thr_id, uint32_t threads, uint32_t startNonce, uint32_t* resNonces, const uint2 highTarget)
 {
-	uint2 highTarget = make_uint2(target_high, target_low);
 	uint32_t tpb;
 	dim3 grid;
 	if (device_sm[device_map[thr_id]] <= 500) {
@@ -284,4 +285,118 @@ void keccak256_cpu_free(int thr_id)
 	cudaFree(d_nonces[thr_id]);
 	//cudaFreeHost(h_nonces[thr_id]);
 	free(h_nonces[thr_id]);
+}
+
+__host__
+int scanhash_keccak256(int thr_id, struct work* work, uint32_t max_nonce, unsigned long *hashes_done)
+{
+	uint32_t endiandata[20];
+	uint32_t *pdata = work->data;
+	uint32_t *ptarget = work->target;
+	const uint32_t first_nonce = pdata[19];
+	const int dev_id = device_map[thr_id];
+	uint32_t throughput;
+	uint32_t intensity = 23;
+    /*
+    if (strstr(device_name[dev_id], "GTX 1070")) intensity = 25;
+    if (strstr(device_name[dev_id], "GTX 1080")) intensity = 26;
+    if (strstr(device_name[dev_id], "RTX 3070")) intensity = 31;
+
+	throughput = cuda_default_throughput(thr_id, 1U << intensity);
+	if (init[thr_id]) throughput = min(throughput, max_nonce - first_nonce);
+
+	if (opt_benchmark)
+		ptarget[7] = 0x000f;
+    */
+
+	if (!init[thr_id])
+	{
+		cudaSetDevice(dev_id);
+        /*
+		if (opt_cudaschedule == -1 && gpu_threads == 1) {
+			cudaDeviceReset();
+			// reduce cpu usage
+			cudaSetDeviceFlags(cudaDeviceScheduleBlockingSync);
+			CUDA_LOG_ERROR();
+		}
+        */
+		cuda_get_arch(thr_id);
+        keccak256_cpu_init(thr_id);
+
+		init[thr_id] = true;
+	}
+
+	for (int k=0; k < 19; k++) {
+		endiandata[i] = pdata[k];
+	}
+
+	const uint2 highTarget = make_uint2(ptarget[6], ptarget[7]);
+    keccak256_setBlock_80((uint64_t*)endiandata);
+    keccak256_setOutput(thr_id);
+
+	do {
+		int order = 0;
+		*hashes_done = pdata[19] - first_nonce + throughput;
+
+        keccak256_cpu_hash_80(thr_id, throughput, pdata[19], work->nonces, highTarget);
+		if (work->nonces[0] != UINT32_MAX)
+		{
+			const uint32_t Htarg = ptarget[7];
+			//uint32_t _ALIGN(64) vhash[8];
+			uint32_t vhash[8];
+
+			endiandata[19] = work->nonces[0];
+			keccak256_hash(vhash, endiandata);
+
+			if (vhash[7] <= ptarget[7] && fulltest(vhash, ptarget)) {
+				work->valid_nonces = 1;
+				work_set_target_ratio(work, vhash);
+				if (!use_compat_kernels[thr_id] && work->nonces[1] != UINT32_MAX) {
+					be32enc(&endiandata[19], work->nonces[1]);
+					keccak256_hash(vhash, endiandata);
+					if (vhash[7] <= ptarget[7] && fulltest(vhash, ptarget)) {
+						work->valid_nonces++;
+						bn_set_target_ratio(work, vhash, 1);
+					}
+					pdata[19] = max(work->nonces[0], work->nonces[1]) + 1;
+				} else {
+					pdata[19] = work->nonces[0] + 1;
+				}
+				return work->valid_nonces;
+			}
+			else if (vhash[7] > Htarg) {
+				gpu_increment_reject(thr_id);
+				if (!opt_quiet)
+				gpulog(LOG_WARNING, thr_id, "result for %08x does not validate on CPU!", work->nonces[0]);
+				pdata[19] = work->nonces[0] + 1;
+				//keccak256_setOutput(thr_id);
+				continue;
+			}
+		}
+
+		if ((uint64_t) throughput + pdata[19] >= max_nonce) {
+			pdata[19] = max_nonce;
+			break;
+		}
+
+		pdata[19] += throughput;
+
+	} while (!work_restart[thr_id].restart);
+
+	*hashes_done = pdata[19] - first_nonce;
+	return 0;
+}
+
+// cleanup
+extern "C" void free_keccak256(int thr_id)
+{
+	if (!init[thr_id])
+		return;
+
+	cudaThreadSynchronize();
+
+    keccak256_cpu_free(thr_id);
+
+	cudaDeviceSynchronize();
+	init[thr_id] = false;
 }
