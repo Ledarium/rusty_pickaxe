e246f87e06d4d8a74ccb630e5d496d94f906e71e
Booba <telegram@amazing_booba>
2021-09-21 14:11:59 +0300

    add other archs

--- build.rs
+++ build.rs
@@ -9,12 +9,10 @@ fn main() {
     cc::Build::new()
         .cuda(true)
         .flag("-cudart=shared")
-        /*
-           .flag("-gencode")
-           .flag("arch=compute_61,code=sm_61") // GTX 10**
-           .flag("-gencode")
-           .flag("arch=compute_75,code=sm_75") // RTX 20**, 1660 Ti
-           */
+        .flag("-gencode")
+        .flag("arch=compute_61,code=sm_61") // GTX 10**
+        .flag("-gencode")
+        .flag("arch=compute_75,code=sm_75") // RTX 20**, 1660 Ti
         .flag("-gencode")
         .flag("arch=compute_86,code=sm_86") // RTX 30**
         .include("./src")
