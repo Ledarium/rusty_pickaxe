6d4b36d81a8f3d2996cef6cb004470d95e7438fe
Booba <telegram@amazing_booba>
2021-09-30 07:50:36 +0300

    add old archs

--- build.rs
+++ build.rs
@@ -11,6 +11,10 @@ fn main() {
         .cuda(true)
         .flag("-cudart=shared")
         .flag("-gencode")
+        .flag("arch=compute_50,code=sm_50") // GTX 7**
+        .flag("-gencode")
+        .flag("arch=compute_52,code=sm_52") // GTX 9**
+        .flag("-gencode")
         .flag("arch=compute_61,code=sm_61") // GTX 10**
         .flag("-gencode")
         .flag("arch=compute_75,code=sm_75") // RTX 20**, 1660 Ti
